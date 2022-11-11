8690c5511634db25f2e343ec73fcf24060fbf9ad
Olga Shvets <olgashvez84@gmail.com>
2022-11-11 14:05:39 +0200

    refactor

--- l9_hw/l9_hw.js
+++ l9_hw/l9_hw.js
@@ -19,8 +19,8 @@
 function sumNumber(number){
  const digits1 = number.slice(0, 3).split("");
  const digits2 = number.slice(3).split("");
- let sum1 = digits1.reduce((i, sum) => i + parseInt(sum), 0);
- let sum2 = digits2.reduce((j, sum) => j + parseInt(sum), 0);
+ let sum1 = digits1.reduce((i, sum) => i + Number(sum), 0);
+ let sum2 = digits2.reduce((j, sum) => j + Number(sum), 0);
  if(sum1 === sum2){
        alert("you are lucky");
  } else{
@@ -34,8 +34,8 @@ function sumNumber(number){
  //Напишіть програму, що отримує у користувача рядок слів та повертає довжину найкоротшого
   //1 variant
 //const str1 = prompt("enter some words");
-const arrWords = str1.split(" ").sort((a, b) => a.length - b.length);
-console.log(arrWords[0]); 
+//const arrWords = str1.split(" ").sort((a, b) => a.length - b.length);
+//console.log(arrWords[0]); 
 
 //2 variant
 // const str2 = prompt("enter some words");
@@ -47,7 +47,7 @@ console.log(arrWords[0]);
  //console.log(shortWord(str2)); 
 
  //3 variant
- const str3 = prompt("enter some words");
+ //const str3 = prompt("enter some words");
  function findShortWord(str3){
     let arrWords = str3.split(" ");
     let shortWord = arrWords.reduce((short, current) => {
@@ -63,10 +63,11 @@ console.log(arrWords[0]);
 //ДНК
 //"ATTGC" // повертає "TAACG"
 //"GTAT" // повертає "CATA" 
-const dna = "GTAT";
+const dna = "CATArrr";
+const dictionary = { A: 'T', T: 'A', C: 'G', G: 'C' };
 //1 variant
 function DNAStandart(dna) {
-    const dictionary = { A: 'T', T: 'A', C: 'G', G: 'C' };
+    
     const arr = dna.split('');
     const arr2 = arr.map((c) => {return dictionary[c]});
     const newStr = arr2.join('');
@@ -74,17 +75,28 @@ function DNAStandart(dna) {
         
 }
 //console.log(DNAStandart(dna));
+
 //2 variant
 function DNAStandart1(dna) {
-    const dictionary = { A: 'T', T: 'A', C: 'G', G: 'C' };
     const arr = dna.split('');
     let arr1 = [];
     for(let i = 0; i < arr.length; i++){
         arr[i] = dictionary[arr[i]];
         arr1.push(arr[i]);
+        
     }
     const newStr = arr1.join('');
-    return newStr;   
-        
+    return newStr;    
+}
+//console.log(DNAStandart1(dna));
+
+//3 variant
+function DNAStandart2(dna) {
+    let newStr ="";
+    for(let i = 0; i < dna.length; i++){
+       
+        newStr += dictionary[dna[i]] ?? '';
+    }
+    return newStr;     
 }
 //console.log(DNAStandart1(dna));
